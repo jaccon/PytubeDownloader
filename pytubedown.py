9f4c07e772a5da8c62fbecbdb53bfb720b4764a6
Jaccon <jaccon@gmail.com>
2019-09-07 01:48:01 -0300

    update feature to download video to Mp4

--- pytubedown.py
+++ pytubedown.py
@@ -4,15 +4,17 @@ print ""
 print " Pytube Downloader "
 ans=True
 while ans:
+
     print("""
     1. Download Youtube URL and convert to MP3
     2. Download Youtube URL to MP4
     3. Exit/Quit
     """)
     ans=raw_input("What would you like to do? ")
+
     if ans=="1":
 
-      print("\n Option 1 choosed...")
+      print("\n 1. Download and convert to MP3 ")
       str1=raw_input("type the Youtube video url and type enter: ")
       
       if str1 != "":
@@ -25,7 +27,12 @@ while ans:
 
     elif ans=="2":
    
-      print("\n Option 2 choosed...")
+      print("\n Download URL to MP4")
+      str2=raw_input("type the Youtube video url and type enter: ")
+
+      if str2 != "":
+        os.system('youtube-dl -f best \'' + str2 + '\'');
+        os.system('youtube-dl -f best --get-description \'' + str2 + '\' > description.txt');
 
     elif ans=="3":
       print("\n Exit choosed... Goodbye") 
